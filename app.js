2cd968cd61c904603158987743b14872d713e67e
Caleb <ogagamaduku@gmail.com>
2023-06-06 13:35:31 +0100

    added mix

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const itemsSchema = new mongoose.Schema({
 const Item = mongoose.model("Item", itemsSchema);
 
 const item1 = new Item({
-  name: "books"
+  name: "bookss"
 });
 
 const item2 = new Item({
